ca743d770f3bb50f82ffae9dd4bc6f8d1644e0d3
AndreyFrol <39055011+AndreyFrol@users.noreply.github.com>
2018-05-07 15:39:14 +0300

    Update FlaskJWT_test.py

--- FlaskJWT_test.py
+++ FlaskJWT_test.py
@@ -1,6 +1,17 @@
-from flask import Flask
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+import sys, os
+import time
+import json
+import RPi.GPIO as GPIO
+from flask import Flask, request, json, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
+import subprocess
+from subprocess import Popen, PIPE, call
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import configparser
 
 
 class User(object):
@@ -47,24 +58,124 @@ def protected():
 
 
 @app.route('/on')
-@jwt_required()
+# @jwt_required()
 def on():
     # insert code for RASPBERRY
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    gpio_pin_number = 12
+    GPIO.setup(gpio_pin_number, GPIO.OUT)
+    GPIO.output(gpio_pin_number, GPIO.HIGH)
     result = {"message": "Device is ON"}
-    return result
+    print("ON")
+    return json.dumps(result)
 
 
 @app.route('/off')
-@jwt_required()
+# @jwt_required()
 def off():
     # insert code for RASPBERRY
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    gpio_pin_number = 12
+    GPIO.setup(gpio_pin_number, GPIO.OUT)
+    GPIO.output(gpio_pin_number, GPIO.LOW)
     result = {"message": "Device is OFF"}
-    return result
+    print("OFF")
+    return json.dumps(result)
 
 
 @app.route('/free')
 def free():
     return "Hello free"
+
+
+@app.route('/humidity', methods=['PUT'])
+@jwt_required()
+def humidity():
+    if request.method == 'PUT':
+        config = configparser.ConfigParser()
+        config.read("/home/pi/poliv/global_config.conf")
+        print("put")
+        data = request.get_json()
+        time = data["freq"]
+        threshold = data["threshold"]
+        irrigation_time = data["irrigation_time"]
+        config.set("time", "time_checking", "%s" % time)
+        config.set("sensor", "critical_position", "%s" % threshold)
+        config.set("time", "time_irrigation", "%s" % irrigation_time)
+        with open("/home/pi/poliv/global_config.conf", "w") as config_file:
+            config.write(config_file)
+        #        os.execv('/home/pi/poliv/autopoliv.py', [' '])
+        #        print("начался полив")
+        response = app.response_class(response=json.dumps(data), status=200, mimetype='application/json')
+        return response
+
+
+@app.route('/pump')
+def pump():
+    Popen("echo 12 > /sys/class/gpio/export", shell=True)
+    txt = '''cat /sys/class/gpio/gpio12/value'''
+    ret = Popen("%s" % txt, shell=True, stdout=PIPE)
+    ret.wait()
+    res = ret.communicate()
+    print(res)
+    print(res[0])
+    res_1 = res[0].decode("utf-8")
+    res_f = res_1.replace("\n", "")
+    print(res_f)
+    res_f = int(res_f)
+    if res_f == 1:
+        #        print("ura")
+        message = {"status": "on"}
+    else:
+        #        print("no")
+        message = {"status": "off"}
+    js = json.dumps(message)
+    response = app.response_class(response=js, status=200, mimetype='application/json')
+    return response
+
+
+@app.route('/irrigation')
+# @jwt_required()
+def irrigation():
+    # insert code for RASPBERRY
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    gpio_pin_number = 12
+    GPIO.setup(gpio_pin_number, GPIO.OUT)
+    GPIO.output(gpio_pin_number, GPIO.HIGH)
+    time.sleep(10)
+    GPIO.output(gpio_pin_number, GPIO.LOW)
+    result = {"message": "The irrigation has been made"}
+    print("ON")
+    return json.dumps(result)
+
+
+@app.route('/show_humidity')
+# @jwt_required()
+def show_humidity():
+    CLK = 11
+    MISO = 9
+    MOSI = 10
+    CS = 8
+    mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+    a = mcp.read_adc(1)
+
+    percent = a * 100 / 1024
+    status = "Inactive" if a == 0 else "Active"
+    mes = {
+        "status": status,
+        "humidity":
+            {
+                "absolute": a,
+                "percent": round(percent, 1)
+            }
+    }
+
+    js = json.dumps(mes)
+    response = app.response_class(response=js, status=200, mimetype='application/json')
+    return response
 
 
 if __name__ == '__main__':
